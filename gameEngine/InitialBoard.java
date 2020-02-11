81aa32273e7c70803cc73eeb73cc979a710899a2
evankuo1 <evankuo@gmail.com>
2020-02-11 13:05:13 -0700

    Pull me into eclipse for the 2nc challenge!

---
+++ gameEngine/InitialBoard.java
@@ -0,0 +1,48 @@
+package gameEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import editMe.Player;
+
+
+public class InitialBoard {
+	
+	int width;
+	int height;
+	List<ArrayList> objList;
+	
+	public InitialBoard() {
+		
+			width = 3;
+			height = 5;
+			objList = new ArrayList<ArrayList>();
+
+			BoardObject player = new Player();
+
+			ArrayList playerList = new ArrayList();
+			playerList.add(0);
+			playerList.add(0);
+			playerList.add(player);
+			objList.add(playerList);
+			
+			Food food = new Food();
+			ArrayList foodList = new ArrayList();
+			foodList.add(2);
+			foodList.add(4);
+			foodList.add(food);
+			objList.add(foodList);
+		}
+	
+	int getWidth() {
+		return width;
+	}
+	
+	int getHeight() {
+		return height;
+	}
+	
+	List<ArrayList> getObjList() {
+		return objList;
+	}
+}
